8a908c5934bf6747720e024532dde37813acda0b
Adriano Baptista <adriano.vb99@gmail.com>
2021-06-16 12:22:21 +0100

    Continue tables.

--- impl/public/js/src/public.js
+++ impl/public/js/src/public.js
@@ -18,13 +18,11 @@ function setupTabs() {
         if (!is_table_profile_create) {
             create_table_profile(data)
             is_table_profile_create = true
-            console.log('create table profile')
         }
     })
 
     document.getElementById('isolate-tab').addEventListener('click', () => {
         if (!is_table_isolate_create) {
-            console.log('create table isolate')
             create_table_isolate(data)
             is_table_isolate_create = true
         }
@@ -141,72 +139,229 @@ function hideDataPart() {
 /** Tables **/
 
 function create_table_profile(data) {
-    console.log(data)
-    console.log('create table')
-    const div = document.getElementById('profileContent')
-    /** Table **/
-    const table = document.createElement('table')
-    table.setAttribute('class', 'table table-bordered table-hover table-responsive')
-    table.setAttribute('height', '450')
-    table.clientHeight = 450
+    const table = document.getElementById('table_profile')
+    table.setAttribute('class', 'table table-bordered table-hover')
+    table.setAttribute('height', "450")
 
     /** head **/
     const head = table.createTHead()
+    head.setAttribute('class', 'table-dark')
     const head_row = head.insertRow()
+    head_row.setAttribute('class', "text-center")
     data.schemeGenes.forEach(row => {
         const cell = head_row.insertCell()
         cell.textContent = row
+        cell.setAttribute('class', 'prof')
+        cell.setAttribute('id', row+'_profile')
         cell.setAttribute('scope', 'col')
+        cell.setAttribute('style',"cursor: pointer")
     })
 
     /** body **/
     const body = table.createTBody()
     data.nodes.forEach(node => {
-        if(node.profile !== undefined) {
+        if (node.profile !== undefined) {
             const body_row = body.insertRow()
-            // const cell_node_name = body_row.insertCell()
-            // cell_node_name.textContent = node.key
+            body_row.setAttribute('class', "text-center")
             const b_cells = node.profile.forEach(profile => {
                 const cell = body_row.insertCell()
                 cell.textContent = profile
+                cell.setAttribute('style',"cursor: pointer")
             })
         }
     })
 
-    /** Append table  **/
-    div.appendChild(table)
+    table.setAttribute('class', 'table table-bordered table-hover')
+    table.setAttribute('height', "450")
+    addListenersToTables()
 }
 
 function create_table_isolate(data) {
-    console.log('create table')
-    const div = document.getElementById('isolateContent')
+
     /** Table **/
-    const table = document.createElement('table')
-    table.setAttribute('class', 'table table-bordered table-hover table-responsive')
-    table.setAttribute('height', '450')
-    table.clientHeight = 450
+    const table = document.getElementById('table_isolate')
 
     /** head **/
     const head = table.createTHead()
+    head.setAttribute('class', 'table-dark')
     const head_row = head.insertRow()
+    head_row.setAttribute('class', "text-center")
     data.metadata.forEach(row => {
         const cell = head_row.insertCell()
         cell.textContent = row
+        cell.setAttribute('class', 'iso')
+        cell.setAttribute('id', row+'_isolate')
         cell.setAttribute('scope', 'col')
+        cell.setAttribute('style',"cursor: pointer")
     })
 
     /** body **/
     const body = table.createTBody()
     data.nodes.forEach(node => {
-        const body_row = body.insertRow()
-        const b_cells = node.isolates[0].forEach(profile => {
-            const cell = body_row.insertCell()
-            cell.textContent = profile
-        })
+        if (node.isolates !== undefined) {
+            const b_cells = node.isolates.forEach(isolate => {
+                const body_row = body.insertRow()
+                body_row.setAttribute('class', "text-center")
+                isolate.forEach(iso => {
+                    const cell = body_row.insertCell()
+                    cell.textContent = iso
+                    cell.setAttribute('style',"cursor: pointer")
+                })
+            })
+        }
     })
 
-    /** Append table  **/
-    div.appendChild(table)
+    table.setAttribute('class', 'table table-bordered table-hover table-responsive')
+    table.setAttribute('height', '450')
+    addListenersToTables()
+}
+
+function addListenersToTables() {
+    if (document.querySelector('.prof') !== null) {
+        document.querySelectorAll('.prof').forEach(elem => {
+            elem.addEventListener('mouseover', () => elem.style.backgroundColor = '#cfcfcf')
+            elem.addEventListener('mouseout', () => elem.style.backgroundColor = '#212529')
+            elem.addEventListener('click', () => clickHeader(elem))
+        })
+    }
+    if (document.querySelector('.iso') !== null) {
+        document.querySelectorAll('.iso').forEach(elem => {
+            elem.addEventListener('mouseover', () => elem.style.backgroundColor = '#cfcfcf')
+            elem.addEventListener('mouseout', () => elem.style.backgroundColor = '#212529')
+            elem.addEventListener('click', () => clickHeader(elem))
+        })
+    }
+}
+
+
+function clickHeader(header) {
+    const HeaderId = header.parentNode.getElementsByTagName('td')[header.id].cellIndex
+    const headerName = header.parentNode.getElementsByTagName('td')[header.id].innerHTML
+    const tdElements = header.parentNode.parentNode.parentNode.lastElementChild.getElementsByTagName('td')
+    const categories = []
+
+    for (let i = 0; i < tdElements.length; i++) {
+        if (tdElements[i].cellIndex === HeaderId) {
+            tdElements[i].style.backgroundColor = '#cfcfcf'
+            categories.push({
+                profile: tdElements[i].parentNode.cells[0].innerText,
+                isolate: tdElements[i].innerText
+            })
+        } else {
+            tdElements[i].style.backgroundColor = '#FFFFFF'
+        }
+    }
+
+    let counts = [];
+    categories.forEach(function (d) {
+        let item = {}
+        if (!counts.find(i => i.name === d.isolate)) {
+            item.name = d.isolate
+            item.value = 0;
+            counts.push(item)
+        }
+        counts.find(i => i.name === d.isolate).value++
+    });
+    const totalLength = counts.length
+    if (counts.length > 20) {
+        counts = counts.slice(0, 19)
+        counts.push({name: 'Others', value: totalLength - 20})
+    }
+    console.log(counts)
+    console.log(headerName)
+    constructPieChart(counts, headerName)
+}
+
+const colorsRange = [
+    "#1b70fc", "#33f0ff", "#718a90", "#b21bff", "#fe6616",
+    "#f9bc0f", "#b65d66", "#07a2e6", "#c091ae", "#10b437",
+    "#ea42fe", "#c281fe", "#4f33ff", "#a946aa", "#16977e",
+    "#a88178", "#5776a9", "#678007", "#fa9316", "#85c070",
+    "#6aa2a9", "#989e5d", "#cd714a", "#c5639c", "#c23271",
+    "#678275", "#c5a121", "#a978ba", "#ee534e", "#d24506",
+    "#6f7385", "#9a634a", "#48aa6f", "#ad9ad0", "#6a8a53",
+    "#8c46fc", "#8f5ab8", "#7133ff", "#d77cd1", "#a9804b",
+    "#a67389", "#9e8cfe", "#bd443c", "#6d63ff", "#d110d5",
+    "#798cc3", "#25b3a7", "#938c6d", "#a05787", "#9c87a0",
+    "#20c773", "#8b696d", "#78762d", "#e154c6", "#40835f",
+    "#d73656", "#1397a3", "#f940a5", "#66aeff", "#d097e7",
+    "#cf7c97", "#8b900a", "#d47270",
+]
+
+function constructPieChart(data, headerName) {
+    if (!d3.select('svg').selectAll('g').empty()) {
+        d3.select('svg').selectAll('g').remove()
+    }
+    const g = d3.select('svg').append('g').attr("transform", `translate(550, 150)`).attr('id', 'pieChart')
+    console.log(g)
+
+    const pie = d3.pie().value(d => d.value)
+
+    const path = d3.arc().outerRadius(150).innerRadius(30)
+
+    const color = d3.scaleOrdinal()
+        .domain(d3.range(0, length))
+        .range(colorsRange)
+
+    const pies = g.selectAll('.arc')
+        .data(pie(data))
+        .enter()
+        .append('g')
+        .classed('arc', true)
+
+    pies.append('path')
+        .attr('d', d => path(d))
+        .attr('fill', (d, i) => color(i))
+        .attr('id', d => d.data.name)
+
+    let colors = []
+    let labels = []
+
+    document.querySelectorAll('.arc').forEach(item => {
+        colors.push(item.getElementsByTagName('path')[0].attributes['fill'].nodeValue)
+    })
+
+    document.querySelectorAll('.arc').forEach(item => {
+        labels.push(item.getElementsByTagName('path')[0].attributes['id'].nodeValue)
+    })
+
+    const pieChart = d3.select('svg').append('g')
+
+    let position = 30
+    data.forEach((item, i) => {
+        pieChart.append('circle')
+            .attr('cy', position)
+            .attr('cx', 760)
+            .attr('r', 6)
+            .style('fill', colors[i])
+
+        pieChart.append('text')
+            .attr('y', position)
+            .attr('x', 770)
+            .text(labels[i])
+            .style("font-size", "15px")
+            .attr("alignment-baseline", "middle")
+
+        position += 20
+    })
+
+    const legend = pieChart.attr('id', 'legend')
+    legend.append('text')
+        .attr('y', 350)
+        .attr('x', 525)
+        .text(headerName)
+        .style("font-size", "15px")
+        .attr("alignment-baseline", "middle")
+
+    legend.append('text')
+        .attr('y', 380)
+        .attr('x', 510)
+        .text('Categories: ' + data.length)
+        .style("font-size", "15px")
+        .attr("alignment-baseline", "middle")
+
+    colors = []
+    labels = []
 }
 
 
@@ -217,15 +372,12 @@ function sendNewickData() {
     let nwk = document.getElementById('formFileNw').files[0]
     nwk.text().then(newick => {
         let body = JSON.stringify({data: newick})
-        console.log('fetch ')
         return fetch('/api/update/newick', {method: 'post', body: body, headers: headers})
             .then(() => {
-                console.log('fetch 2')
                 return fetch('/api/data', {headers: headers})
                     .then(async res => {
                         if (res.status === 500) alertMsg('error')
                         data = await res.json()
-                        console.log(data)
                     })
                     .catch(err => alertMsg(err))
             })
@@ -247,7 +399,6 @@ function sendProfileData() {
                 .then(async res => {
                     if (res.status === 500) alertMsg('error')
                     data = await res.json()
-                    console.log(data)
                 })
                 .catch(err => alertMsg(err))
         }).catch(err => alertMsg(err))
@@ -268,7 +419,6 @@ function sendIsolateData() {
                 .then(async res => {
                     if (res.status === 500) alertMsg('error')
                     data = await res.json()
-                    console.log(data)
                 })
                 .catch(err => alertMsg(err))
         }).catch(err => alertMsg(err))

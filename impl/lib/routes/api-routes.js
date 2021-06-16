7af451338e34ae1f011375adefc9cb3b3f7c37dc
Adriano Baptista <adriano.vb99@gmail.com>
2021-06-16 12:21:20 +0100

    Remove comments in api-routes.

--- impl/lib/routes/api-routes.js
+++ impl/lib/routes/api-routes.js
@@ -1,4 +1,3 @@
-const nwkParser = require('../service/parsing/newickParser')
 const render = require('../service/data_rendering')
 const Router = require('express').Router
 const router = Router()
@@ -8,26 +7,15 @@ let profiles
 let isolates
 
 router.get('/data', (req, res, next) =>{
-    //console.log('render')
     render.getRenderData().then(data => {
-        console.log(data)
         res.json(data)
     })
-    //console.log('render over')
 })
 
 router.post('/update/newick', (req, res, next) => {
     let newick = req.body.data;
     tree = render.set_tree_data(newick)
-
     res.json()
-    // todo was not here
-    // render.getRenderData().then(data => {
-    //    console.log(data)
-    //    res.json(data)
-    // })
-
-
 })
 
 router.post('/update/profiles', (req, res, next) =>{

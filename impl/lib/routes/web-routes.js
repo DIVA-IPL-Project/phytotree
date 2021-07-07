15b5e70993fc69d3189296398f9892f9ff481ebd
Adriano Baptista <adriano.vb99@gmail.com>
2021-07-07 16:16:30 +0100

    Start cliente and server side.

--- impl/lib/routes/web-routes.js
+++ impl/lib/routes/web-routes.js
@@ -2,33 +2,46 @@ const path = require('path');
 const Router = require('express').Router
 const router = Router()
 
-let profiles
-let isolates
+let view_path
 
-router.get('/home', (req, res, next) => {
-    res.sendFile(path.resolve('impl/lib/views/home.html'));
-})
-
-router.post('/profiles', (req, res, next) => {
-    profiles = req.body
-    res.render("profiles", {"profiles": profiles})
-})
-
-router.get('/profiles', (req, res, next) => {
-    if (profiles) {
-        res.render("profiles", {"header": profiles.header, "prof": profiles.prof})
+function init_path(path){
+    view_path = path
+    return {
+        init_router
     }
-})
+}
 
-router.post('/isolates', (req, res, next) => {
-    isolates = req.body
-    res.render("profiles", {"profiles": isolates})
-})
 
-router.get('/isolates', (req, res, next) => {
-    if (isolates) {
-        res.render("isolates", {"header": isolates.header, "iso": isolates.iso})
-    }
-})
+function init_router(){
+    let profiles
+    let isolates
 
-module.exports = router;
+    router.get('/home', (req, res, next) => {
+        res.sendFile(path.resolve(view_path));
+    })
+
+    router.post('/profiles', (req, res, next) => {
+        profiles = req.body
+        res.render("profiles", {"profiles": profiles})
+    })
+
+    router.get('/profiles', (req, res, next) => {
+        if (profiles) {
+            res.render("profiles", {"header": profiles.header, "prof": profiles.prof})
+        }
+    })
+
+    router.post('/isolates', (req, res, next) => {
+        isolates = req.body
+        res.render("profiles", {"profiles": isolates})
+    })
+
+    router.get('/isolates', (req, res, next) => {
+        if (isolates) {
+            res.render("isolates", {"header": isolates.header, "iso": isolates.iso})
+        }
+    })
+    return router
+}
+
+module.exports = {init_path};
--- impl/server.js
+++ impl/server.js
@@ -2,18 +2,46 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const routesApi = require('./lib/routes/api-routes');
 const routesWeb = require('./lib/routes/web-routes');
-
-const app = express();
 const PORT = 8000;
+const app = express();
 
-app.use('/public', express.static(__dirname + '/public'));
 
-app.use(bodyParser.json({limit: "50mb"}))
+function build_app(arg){
+    switch (arg) {
+        case '-server': {
 
-app.use('/api', routesApi)
-app.use(routesWeb)
+            app.use('/public', express.static(__dirname + '/public'));
 
-app.listen(PORT, function (err) {
-    if (err) console.log(err);
-    console.log("Server listening on PORT", PORT);
-})
+            app.use(bodyParser.json({limit: "50mb"}))
+
+            app.use('/api', routesApi)
+            app.use(routesWeb.init_path('lib/views/home.html').init_router())
+
+            app.listen(PORT, function (err) {
+                if (err) console.log(err);
+                console.log("Server listening on PORT", PORT);
+            })
+            break
+        }
+        case '-client': {
+            //console.log('No implemented yet')
+            app.use('/public', express.static(__dirname + '/public_cs'));
+
+            app.use(bodyParser.json({limit: "50mb"}))
+
+            app.use('/api', routesApi)
+            app.use(routesWeb.init_path('lib/views/home_cs.html').init_router())
+
+            app.listen(PORT, function (err) {
+                if (err) console.log(err);
+                console.log("Server listening on PORT", PORT);
+            })
+            break
+        }
+        default:
+            break
+    }
+}
+
+module.exports = {build_app}
+

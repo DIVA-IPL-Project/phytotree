23e1d214cddb0f729ffc7f1576998721712f63dc
Francisco <a46018@alunos.isel.pt>
2021-07-13 20:06:07 +0100

    fixes path errors and adds build for linux

--- App/server/server.js
+++ App/server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser')
 const routesApi = require('./lib/routes/api-routes');
@@ -10,13 +11,14 @@ function build_app(arg){
     switch (arg) {
         case '-server': {
 
-            app.use('/public', express.static(__dirname + '/public'))
+            app.use('/public', express.static(path.join(__dirname, 'public')))
 
             app.use(bodyParser.json({limit: "50mb"}))
 
             app.use('/api', routesApi)
 
-            app.use(routesWeb.init_path(__dirname + '\\lib\\views\\home.html').init_router())
+            let homeUri = path.join(__dirname, 'lib', 'views', 'home.html')
+            app.use(routesWeb.init_path(homeUri).init_router())
 
             app.listen(PORT, function (err) {
                 if (err) console.log(err);
@@ -27,12 +29,14 @@ function build_app(arg){
         }
         case '-client': {
 
-            app.use('/public', express.static(__dirname + '/public'))
+            app.use('/public', express.static(path.join(__dirname, 'public')))
 
             app.use(bodyParser.json({limit: "50mb"}))
 
             app.use('/api', routesApi)
-            app.use(routesWeb.init_path(__dirname + '\\lib\\views\\home_cs.html').init_router())
+
+            let homeUri = path.join(__dirname, 'lib', 'views', 'home.html')
+            app.use(routesWeb.init_path(homeUri).init_router())
 
             app.listen(PORT, function (err) {
                 if (err) console.log(err);

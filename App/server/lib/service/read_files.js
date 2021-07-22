060188f65eef7b6f2bc19a2511f7a7a14ec8d360
Francisco <a46018@alunos.isel.pt>
2021-07-22 15:46:22 +0100

    corrects error on read_files

--- App/server/lib/service/read_files.js
+++ App/server/lib/service/read_files.js
@@ -1,15 +1,16 @@
 const fs = require('fs/promises')
+const path = require('path');
 
 function read_tree_file(){
-    return fs.readFile(__dirname + '\\test_files\\nwk.txt')
+    return fs.readFile(path.join(__dirname, 'test_files', 'nwk.txt'))
 }
 
 function read_profile_file(){
-    return fs.readFile(__dirname + '\\test_files\\profiles.tab')
+    return fs.readFile(path.join(__dirname, 'test_files', 'profiles.tab'))
 }
 
 function read_isolate_file(){
-    return fs.readFile(__dirname + '\\test_files\\isolates.tab')
+    return fs.readFile(path.join(__dirname, 'test_files', 'isolates.tab'))
 }
 
 module.exports = {

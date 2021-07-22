0132084cf689e0aea2be3884835b24ad4b83c03d
Adriano Baptista <adriano.vb99@gmail.com>
2021-07-22 20:02:12 +0100

    Correct web routes

--- App/server/lib/routes/web-routes.js
+++ App/server/lib/routes/web-routes.js
@@ -13,34 +13,11 @@ function init_path(path){
 
 
 function init_router(){
-    let profiles
-    let isolates
 
     router.get('/home', (req, res, next) => {
         res.sendFile(path.resolve(view_path));
     })
 
-    router.post('/profiles', (req, res, next) => {
-        profiles = req.body
-        res.render("profiles", {"profiles": profiles})
-    })
-
-    router.get('/profiles', (req, res, next) => {
-        if (profiles) {
-            res.render("profiles", {"header": profiles.header, "prof": profiles.prof})
-        }
-    })
-
-    router.post('/isolates', (req, res, next) => {
-        isolates = req.body
-        res.render("profiles", {"profiles": isolates})
-    })
-
-    router.get('/isolates', (req, res, next) => {
-        if (isolates) {
-            res.render("isolates", {"header": isolates.header, "iso": isolates.iso})
-        }
-    })
     return router
 }
 
